8d895bb6044326b6a1a92e5f9c74525f5f7475e2
Christos Themelis <themelisx@gmail.com>
2025-10-30 10:42:14 +0200

    initial commit

---
+++ src/myDebug.cpp
@@ -0,0 +1,200 @@
+#include <Arduino.h>
+#include "myDebug.h"
+
+MyDebug::MyDebug() {   
+
+};
+
+void MyDebug::start(int speed, int debugLevel) {
+
+    this->speed = speed;
+    this->debugLevel = debugLevel;
+    if (debugLevel != DEBUG_LEVEL_NONE) {        
+        Serial.begin(speed);
+    }
+    
+}
+
+void MyDebug::print(int debugLevel, const __FlashStringHelper *ifsh) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(ifsh);
+    }
+}
+
+void MyDebug::print(int debugLevel, const String &s) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(s);
+    }
+}
+
+void MyDebug::print(int debugLevel, const char str[]) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(str);
+    }
+}
+
+void MyDebug::print(int debugLevel, char c) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(c);
+    }
+}
+
+void MyDebug::print(int debugLevel, unsigned char b, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(b, base);
+    }
+}
+
+void MyDebug::print(int debugLevel, int n, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(n, base);
+    }
+}
+
+void MyDebug::print(int debugLevel, int num) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(num);
+    }
+}
+
+void MyDebug::print(int debugLevel, unsigned int n, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(n, base);
+    }
+}
+
+void MyDebug::print(int debugLevel, long n, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(n, base);
+    }
+}
+
+void MyDebug::print(int debugLevel, unsigned long n, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(n, base);
+    }
+}
+
+void MyDebug::print(int debugLevel, long long n, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(n, base);
+    }
+}
+
+void MyDebug::print(int debugLevel, unsigned long long n, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(n, base);
+    }
+}
+
+void MyDebug::print(int debugLevel, double n, int digits) {
+   if (debugLevel <= this->debugLevel) {
+        Serial.print(n, digits);
+    }
+}
+
+#ifdef ESP32
+void MyDebug::print(int debugLevel, const Printable& x) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(x);
+    }
+}
+
+void MyDebug::print(int debugLevel, struct tm * timeinfo, const char * format) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.print(timeinfo, format);
+    }
+}
+#endif
+
+void MyDebug::println(int debugLevel, const __FlashStringHelper *ifsh) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(ifsh);
+    }
+}
+
+void MyDebug::println(int debugLevel, const String &s) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(s);
+    }
+}
+
+void MyDebug::println(int debugLevel, const char c[]) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(c);
+    }
+}
+
+void MyDebug::println(int debugLevel, char c) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(c);
+    }
+}
+
+void MyDebug::println(int debugLevel, int num) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num);
+    }
+}
+
+void MyDebug::println(int debugLevel, unsigned char b, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(b, base);
+    }
+}
+
+void MyDebug::println(int debugLevel, int num, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num, base);
+    }
+}
+
+void MyDebug::println(int debugLevel, unsigned int num, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num, base);
+    }
+}
+
+void MyDebug::println(int debugLevel, long num, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num, base);
+    }
+}
+
+void MyDebug::println(int debugLevel, unsigned long num, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num, base);
+    }
+}
+
+void MyDebug::println(int debugLevel, long long num, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num, base);
+    }
+}
+
+void MyDebug::println(int debugLevel, unsigned long long num, int base) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num, base);
+    }
+}
+
+void MyDebug::println(int debugLevel, double num, int digits) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(num, digits);
+    }
+}
+
+#ifdef ESP32
+void MyDebug::println(int debugLevel, const Printable& x) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(x);
+    }
+}
+
+void MyDebug::println(int debugLevel, struct tm * timeinfo, const char * format) {
+    if (debugLevel <= this->debugLevel) {
+        Serial.println(timeinfo, format);
+    }
+}
+#endif
---
+++ src/myDebug.h
@@ -0,0 +1,61 @@
+#ifndef DEBUG_h
+#define DEBUG_h
+
+#include <Arduino.h>
+
+#define DEBUG_LEVEL_NONE 0
+#define DEBUG_LEVEL_ERROR 1
+#define DEBUG_LEVEL_WARNING 2
+#define DEBUG_LEVEL_INFO 3
+#define DEBUG_LEVEL_DEBUG 4
+#define DEBUG_LEVEL_DEBUG2 5
+
+class MyDebug {
+  private:
+    int speed;
+    int debugLevel;
+
+  public:
+    MyDebug();
+    void start(int speed, int debugLevel);
+
+    void print(int debugLevel, const __FlashStringHelper *ifsh);
+    void print(int debugLevel, const String &s);
+    void print(int debugLevel, const char str[]);
+    void print(int debugLevel, char c);
+    void print(int debugLevel, unsigned char b, int base);
+    void print(int debugLevel, int num);
+    void print(int debugLevel, int n, int base);
+    void print(int debugLevel, unsigned int n, int base);
+    void print(int debugLevel, long n, int base);
+    void print(int debugLevel, unsigned long n, int base);
+    void print(int debugLevel, long long n, int base);
+    void print(int debugLevel, unsigned long long n, int base);
+    void print(int debugLevel, double n, int digits);    
+    #ifdef ESP32
+      void print(int debugLevel, const Printable& x);
+      void print(int debugLevel, struct tm * timeinfo, const char * format);
+    #endif
+
+    void println(int debugLevel, const __FlashStringHelper *ifsh);
+    void println(int debugLevel, const String &s);
+    void println(int debugLevel, const char c[]);
+    void println(int debugLevel, char c);
+    void println(int debugLevel, int num);
+    void println(int debugLevel, unsigned char b, int base);
+    void println(int debugLevel, int num, int base);
+    void println(int debugLevel, unsigned int num, int base);
+    void println(int debugLevel, long num, int base);
+    void println(int debugLevel, unsigned long num, int base);
+    void println(int debugLevel, long long num, int base);
+    void println(int debugLevel, unsigned long long num, int base);
+    void println(int debugLevel, double num, int digits);
+    
+    #ifdef ESP32
+      void println(int debugLevel, const Printable& x);
+      void println(int debugLevel, struct tm * timeinfo, const char * format);
+    #endif
+
+};
+
+#endif
